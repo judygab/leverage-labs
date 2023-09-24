c950b194f55c8cf824eed06ca67836a62616553e
judygab <ivditigabeskiria@gmail.com>
2023-09-24 01:45:03 -0400

    added auctions api

--- components/MessageBot.tsx
+++ components/MessageBot.tsx
@@ -7,12 +7,18 @@ type Props = {
 
 const MessageBot = ({ close }: Props) => {
   return (
-    <div className='bg-[#D9D9D9] p-8 rounded-[45px] rounded-br-none absolute  bottom-40 right-6 z-50 border-black'>
-      <div className='flex content-between items-start'>
+    <div className='bg-[#D9D9D9] p-8 rounded-[45px] rounded-br-none absolute  bottom-40 right-6 z-50'>
+      <div className='flex content-between items-start border-b-2 border-black'>
         <p className='font-bold min-w-[160px] max-w-lg text-black'>All notifications</p>
         <Button onClick={close} bgColor="transparent" w="26px" justifySelf="start" justifyContent="end" alignItems="start" padding={0}>
           <Image aria-label="close" src={"./x-rounded.svg"} />
         </Button></div>
+      <ul>
+        <li className='border-b border-black'>18th June 2022: Action Started</li>
+        <li className='border-b border-black'>17th July 2022: Action Started</li>
+        <li className='border-b border-black'>16th August 2022: Action Started</li>
+        <li className='border-b border-black'>15th September 2022: Action Started</li>
+      </ul>
       <Divider />
     </div>
   )
