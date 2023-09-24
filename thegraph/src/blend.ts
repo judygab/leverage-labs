732e2076f8069ee16f7109a100bfe63b031069b0
Ali Kim <alikim@gmail.com>
2023-09-23 20:17:33 -0400

    theGraph: stuck with MemoryAccessError related to pushing loanId to array in handler

--- thegraph/src/blend.ts
+++ thegraph/src/blend.ts
@@ -20,64 +20,6 @@ import {
 
 import { Lien, Loan } from "../generated/schema"
 
-// export function handleAdminChanged(event: AdminChanged): void {
-//   // Entities can be loaded from the store using a string ID; this ID
-//   // needs to be unique across all entities of the same type
-//   let entity = ExampleEntity.load(event.transaction.from)
-
-//   // Entities only exist after they have been saved to the store;
-//   // `null` checks allow to create entities on demand
-//   if (!entity) {
-//     entity = new ExampleEntity(event.transaction.from)
-
-//     // Entity fields can be set using simple assignments
-//     entity.count = BigInt.fromI32(0)
-//   }
-
-//   // BigInt and BigDecimal math are supported
-//   entity.count = entity.count + BigInt.fromI32(1)
-
-//   // Entity fields can be set based on event parameters
-//   entity.previousAdmin = event.params.previousAdmin
-//   entity.newAdmin = event.params.newAdmin
-
-//   // Entities can be written to the store with `.save()`
-//   entity.save()
-
-//   // Note: If a handler doesn't require existing field values, it is faster
-//   // _not_ to load the entity from the store. Instead, create it fresh with
-//   // `new Entity(...)`, set the fields that should be updated and save the
-//   // entity back to the store. Fields that were not set or unset remain
-//   // unchanged, allowing for partial updates to be applied.
-
-//   // It is also possible to access smart contracts from mappings. For
-//   // example, the contract that has emitted the event can be connected to
-//   // with:
-//   //
-//   // let contract = Contract.bind(event.address)
-//   //
-//   // The following functions can then be called on this contract to access
-//   // state variables and other data:
-//   //
-//   // - contract.amountTaken(...)
-//   // - contract.blockRange(...)
-//   // - contract.borrow(...)
-//   // - contract.buyToBorrow(...)
-//   // - contract.buyToBorrowLocked(...)
-//   // - contract.buyToBorrowV2(...)
-//   // - contract.cancelledOrFulfilled(...)
-//   // - contract.getOfferHash(...)
-//   // - contract.getOracleOfferHash(...)
-//   // - contract.getSellOfferHash(...)
-//   // - contract.information(...)
-//   // - contract.liens(...)
-//   // - contract.nonces(...)
-//   // - contract.onERC721Received(...)
-//   // - contract.oracles(...)
-//   // - contract.owner(...)
-//   // - contract.pendingOwner(...)
-//   // - contract.proxiableUUID(...)
-// }
 
 export function handleLoanOfferTaken(event: LoanOfferTaken): void {
 
@@ -103,6 +45,7 @@ export function handleLoanOfferTaken(event: LoanOfferTaken): void {
 
     let loan = new Loan(`${lender.toHexString()}-${rate}-${loanAmount}`)
     loan.lien = lienId
+    loan.lienId = lienId
     loan.lender = lender
     loan.loanAmount = loanAmount
     loan.rate = rate
@@ -111,14 +54,14 @@ export function handleLoanOfferTaken(event: LoanOfferTaken): void {
 
     lien.loans = [loan.id]
     lien.save()
-  } else {
+  }
 
+  else {
 
     let loans = lien.loans
 
     for (let i = 0; i < loans.length; i++) {
-      let loan = Loan.load(loans[i])
-      let item = loan
+      let item = Loan.load(loans[i])
 
       if (item) {
         // scenario 2: same term. update loan amount (borrower paid back some money)
@@ -127,20 +70,25 @@ export function handleLoanOfferTaken(event: LoanOfferTaken): void {
           if (loan) {
             loan.loanAmount = loanAmount
             loan.save()
-            break;
           }
         } else {
-          // scenario 3: some term changed. create new loan. 
-          let loan = new Loan(`${lender.toHexString()}-${rate}-${loanAmount}`)
+          // scenario 3: some term changed. create new loan.
+          let loanid = `${lender.toHexString()}-${rate}-${loanAmount}`
+          let loan = new Loan(loanid)
           loan.lien = lienId
+          loan.lienId = lienId
           loan.lender = lender
           loan.loanAmount = loanAmount
           loan.rate = rate
           loan.startTime = event.block.timestamp
           loan.save()
 
-          loans.push(loan.id)
+          loans.push(loanid) // comment out for v16. no error v16.
           lien.loans = loans
+
+          //reset auction start, assuming new loan was taken on existing lien
+          lien.auctionStarted = null
+
           lien.save()
         }
       }
@@ -152,11 +100,38 @@ export function handleLoanOfferTaken(event: LoanOfferTaken): void {
 
 }
 
-export function handleRepay(event: Repay): void { }
 
-export function handleSeize(event: Seize): void { }
+// track when repay event was triggered
+export function handleRepay(event: Repay): void {
+  // const lienId = event.params.lienId
 
-export function handleStartAuction(event: StartAuction): void { }
+  // let lien = Lien.load(lienId.toString())
+  // if (lien) {
+  //   lien.repayTime = event.block.timestamp
+  //   lien.save()
+  // }
+}
+
+// track when SEIZE event was triggered
+export function handleSeize(event: Seize): void {
+  // const lienId = event.params.lienId
+
+  // let lien = Lien.load(lienId.toString())
+  // if (lien) {
+  //   lien.seizeTime = event.block.timestamp
+  //   lien.save()
+  // }
+}
+
+export function handleStartAuction(event: StartAuction): void {
+  // const lienId = event.params.lienId
+
+  // let lien = Lien.load(lienId.toString())
+  // if (lien) {
+  //   lien.auctionStarted = event.block.timestamp
+  //   lien.save()
+  // }
+}
 
 
 
